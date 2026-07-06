b5bc19d863009f9fe1abe42c0813a3ed828b3dfe
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "eventgrid_domain_topics" {
   description = "All eventgrid_domain_topic resources"
   value       = azurerm_eventgrid_domain_topic.eventgrid_domain_topics
 }
+output "eventgrid_domain_topics_domain_name" {
+  description = "List of domain_name values across all eventgrid_domain_topics"
+  value       = [for k, v in azurerm_eventgrid_domain_topic.eventgrid_domain_topics : v.domain_name]
+}
+output "eventgrid_domain_topics_name" {
+  description = "List of name values across all eventgrid_domain_topics"
+  value       = [for k, v in azurerm_eventgrid_domain_topic.eventgrid_domain_topics : v.name]
+}
+output "eventgrid_domain_topics_resource_group_name" {
+  description = "List of resource_group_name values across all eventgrid_domain_topics"
+  value       = [for k, v in azurerm_eventgrid_domain_topic.eventgrid_domain_topics : v.resource_group_name]
+}
 
